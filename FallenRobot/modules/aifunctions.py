6e8c1fb6616e8be351add76c8858be7bd8aaf5f9
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2022-10-20 14:30:36 +0530

    Update aifunctions.py

--- FallenRobot/modules/aifunctions.py
+++ FallenRobot/modules/aifunctions.py
@@ -28,7 +28,7 @@ async def hmm(event):
     lodu = response.json()
     result = lodu["message"]["text"]
     if "no no" in result:
-        pro = "I am fairly found and I was made by @anonymous_was_bot."
+        pro = "I am fairly found and I was made by @AerodynamicV1_Official."
         try:
             async with telethn.action(event.chat_id, "typing"):
                 await asyncio.sleep(2)
